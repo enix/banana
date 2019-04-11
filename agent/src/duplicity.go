23cc1bf8c0289454be7ce87fef7e6124e3d5d888
Arthur Chaloin <arthur@achaloin.com>
2019-04-11 17:49:51 +0200

    fix(agent): remove useless/crititcal logs

--- agent/src/duplicity.go
+++ agent/src/duplicity.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 // DuplicityBackend : BackupBackend implementation for duplicity
@@ -11,8 +10,6 @@ type DuplicityBackend struct{}
 // Backup : BackupBackend's Backup call implementation for duplicity
 func (d *DuplicityBackend) Backup(config *Config, cmd *BackupCmd) error {
 	endpoint := fmt.Sprintf("s3://%s/%s/%s", config.StorageHost, config.BucketName, cmd.Name)
-	fmt.Printf("%+v", config)
-	fmt.Println(os.Getenv("AWS_ACCESS_KEY_ID"))
 	fmt.Printf("+ duplicity --full-if-older-than 1W %s %s\n", cmd.Target, endpoint)
 	return Execute("duplicity", "--full-if-older-than", "1W", cmd.Target, endpoint)
 }
