04c91547ac90991a01a9d4286037b99a3ce9b46a
Arthur Chaloin <arthur@achaloin.com>
2019-04-12 18:11:28 +0200

    fix(api): minor time formart fix in logger

--- api/src/logger/logger.go
+++ api/src/logger/logger.go
@@ -10,7 +10,7 @@ import (
 
 // Log : Log message to stdout with LOG level
 func Log(format string, args ...interface{}) (int, error) {
-	return fmt.Printf(("[" + time.Now().String() + "][LOG] " + format + "\n"), args...)
+	return fmt.Printf(("[" + time.Now().Format("Mon Jan 2 15:04:05") + "][LOG] " + format + "\n"), args...)
 }
 
 // LogError : Log error to stderr with ERROR level
