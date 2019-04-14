c91931590b1ba76ee7e4f9608c61d0ce4b853ade
Arthur Chaloin <arthur@achaloin.com>
2019-04-14 15:07:42 +0200

    feat(api): serve agents list

--- api/src/routes/agent.go
+++ api/src/routes/agent.go
@@ -10,11 +10,16 @@ import (
 
 // RegisterAgent : Add a new agent to the agent list
 func RegisterAgent(context *gin.Context, issuer *RequestIssuer) (int, interface{}) {
-	agent := models.NewAgent(issuer.Organization, issuer.CommonName)
+	agent := models.NewAgent(issuer.Organization, issuer.Organization)
 	services.DbSet(agent.GetFullKey(), agent)
 
 	agents := services.DbGet("agents").([]models.Agent)
 	agents = append(agents, *agent)
 	services.DbSet("agents", agents)
 	return http.StatusOK, agent
+}
+
+// ServeAgentList : Returns the agent list
+func ServeAgentList(context *gin.Context, issuer *RequestIssuer) (int, interface{}) {
+	return http.StatusOK, services.DbGet("agents")
 }
