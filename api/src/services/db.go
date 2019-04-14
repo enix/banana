d3e1025bff653da11eae163111cf3c53c00c947c
Arthur Chaloin <arthur@achaloin.com>
2019-04-14 16:24:54 +0200

    fix(api): add check for empty array in redis as go unmarshall them as nil pointers...

--- api/src/services/db.go
+++ api/src/services/db.go
@@ -3,6 +3,7 @@ package services
 import (
 	"encoding/json"
 	"errors"
+	"os"
 
 	"enix.io/banana/src/logger"
 	"github.com/go-redis/redis"
@@ -24,6 +25,13 @@ func DbSet(key string, value interface{}) error {
 // DbGet : Convenience function to avoid manual JSON decoding
 func DbGet(key string) (out interface{}) {
 	str := Db.Get(key).Val()
+
+	if str == "[]" {
+		// yes go does not handle that... https://stackoverflow.com/a/33183170
+		out = make([]interface{}, 0)
+		return
+	}
+
 	json.Unmarshal([]byte(str), out)
 	return
 }
@@ -32,14 +40,19 @@ func DbGet(key string) (out interface{}) {
 //													Calls such as DbGet and DbSet will crash if called before this
 func OpenDatabaseConnection() error {
 	Db = redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "",
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASSWD"),
 		DB:       0,
 	})
 
 	pong, err := Db.Ping().Result()
 	if err != nil || pong != "PONG" {
 		return errors.New("failed to connect to redis database")
+	}
+
+	agents := DbGet("agents")
+	if agents == nil {
+		DbSet("agents", make([]interface{}, 0))
 	}
 
 	logger.Log("etablished connection with redis database")
