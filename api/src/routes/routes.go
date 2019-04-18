9ac25ef020e8b5af5e15b3527be1cedd96f68ea8
Arthur Chaloin <arthur@achaloin.com>
2019-04-18 15:10:19 +0200

    refactor(api): remove a lot of useless stuff

--- api/src/routes/routes.go
+++ api/src/routes/routes.go
@@ -5,10 +5,6 @@ import "github.com/gin-gonic/gin"
 // InitializeRoutes : All API endpoints
 func InitializeRoutes(router *gin.Engine) {
 	router.GET("/ping", handleClientRequest(handlePingRequest))
-	router.POST("/ping", handleSignedRequest(handlePingRequest))
-	router.GET("/containers", handleClientRequest(ServeBackupContainerList))
-	router.GET("/containers/:containerName", handleClientRequest(ServeBackupContainer))
-	router.GET("/containers/:containerName/tree/:treeName", handleClientRequest(ServeBackupTree))
 	router.GET("/agents", handleClientRequest(ServeAgentList))
 	router.POST("/agents", handleClientRequest(RegisterAgent))
 	router.POST("/agents/notify", handleClientRequest(ReceiveAgentMesssage))
