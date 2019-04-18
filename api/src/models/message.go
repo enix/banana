edd18a46dbba78de2ba5d406016e5d49ab917366
Arthur Chaloin <arthur@achaloin.com>
2019-04-18 15:00:31 +0200

    feat(api): abstract message signature/verification inside the message struct itself

--- api/src/models/message.go
+++ api/src/models/message.go
@@ -1,24 +1,28 @@
 package models
 
 import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
 	"fmt"
+
+	"enix.io/banana/src/logger"
+	"enix.io/banana/src/services"
 )
 
 // Message : Representation of an agent's notification
 type Message struct {
-	SenderID  string                 `json:"sender_id"`
 	Version   int8                   `json:"version"`
 	Timestamp int64                  `json:"timestamp"`
+	SenderID  string                 `json:"sender_id"`
+	Signature string                 `json:"signature,omitempty"`
 	Data      map[string]interface{} `json:"data"`
-}
-
-// NewMessage : Convenience function for creating a message
-func NewMessage(timestamp int64, data map[string]interface{}) *Message {
-	return &Message{
-		Version:   1,
-		Timestamp: timestamp,
-		Data:      data,
-	}
 }
 
 // GetSortedSetScore : Generate the score that will be used to store within redis sorted set
@@ -29,4 +33,39 @@ func (msg *Message) GetSortedSetScore() float64 {
 // GetFullKey : Get the key to be used within redis
 func (msg *Message) GetFullKey() string {
 	return fmt.Sprintf("messages:%s", msg.SenderID)
+}
+
+// VerifySignature : Verify that the signature match the message's content
+func (msg *Message) VerifySignature(cert string) error {
+	sig := msg.Signature
+	msg.Signature = ""
+	rawMessage, _ := json.Marshal(msg)
+	err := services.VerifySha256Signature(rawMessage, sig, cert)
+	if err != nil {
+		logger.LogError(err)
+		return errors.New("invalid signature")
+	}
+	msg.Signature = sig
+	return nil
+}
+
+// Sign : Marshal the struct and generate signature from the result
+func (msg *Message) Sign(pemEncodedPrivateKey string) error {
+	rawMessage, _ := json.Marshal(msg)
+	hash := sha256.New()
+	hash.Write(rawMessage)
+	digest := hash.Sum(nil)
+	block, _ := pem.Decode([]byte(pemEncodedPrivateKey))
+	privkey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return fmt.Errorf("failed to load private key: %s", err)
+	}
+	sig, err := rsa.SignPKCS1v15(rand.Reader, privkey, crypto.SHA256, digest)
+	if err != nil {
+		return err
+	}
+	base64sig := make([]byte, base64.StdEncoding.EncodedLen(len(sig)))
+	base64.StdEncoding.Encode(base64sig, sig)
+	msg.Signature = string(base64sig)
+	return err
 }
