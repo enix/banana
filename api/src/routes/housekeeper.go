5ff102866e9dd81997eeec37ca5e2f7df507d8ef
Arthur Chaloin <arthur@achaloin.com>
2019-04-25 16:25:45 +0200

    feat(api): add websocket route to listen for agent messages

---
+++ api/src/routes/housekeeper.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"fmt"
+
+	"enix.io/banana/src/models"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var houseKeeperEvents = make(chan models.Message)
+
+var wsUpgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// HandleHouseKeeperConnection : Upgrade HTTP to WS and send events to the housekeeper
+func handleHouseKeeperConnection(context *gin.Context) {
+	conn, err := wsUpgrader.Upgrade(context.Writer, context.Request, nil)
+	if err != nil {
+		fmt.Printf("failed to set websocket upgrade: %+v\n", err)
+		return
+	}
+
+	for {
+		conn.WriteJSON(<-houseKeeperEvents)
+	}
+}
--- api/src/routes/routes.go
+++ api/src/routes/routes.go
@@ -1,12 +1,30 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"enix.io/banana/src/models"
+	"github.com/gin-gonic/gin"
+)
 
 // InitializeRoutes : All API endpoints
 func InitializeRoutes(router *gin.Engine) {
 	router.GET("/ping", handleClientRequest(handlePingRequest))
 	router.GET("/agents", handleClientRequest(ServeAgentList))
-	router.POST("/agents/notify", handleClientRequest(ReceiveAgentMesssage))
 	router.GET("/agents/:id", handleClientRequest(ServeAgent))
 	router.GET("/agents/:id/messages", handleClientRequest(ServeAgentMesssages))
+	router.POST("/agents/notify", handleClientRequest(ReceiveAgentMesssage))
+	router.GET("/housekeeper/ws", handleHouseKeeperConnection)
+	router.GET("/test", func(context *gin.Context) {
+		msg := models.Message{
+			SenderID: "test",
+		}
+
+		// select {
+		houseKeeperEvents <- msg
+		// 	fmt.Println("sent message", msg)
+		// default:
+		// 	fmt.Println("no message sent")
+		// }
+
+		context.JSON(200, msg)
+	})
 }
