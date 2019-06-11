fe206f65c4cb96262ffd125df6273edbff9d9b28
Arthur Chaloin <arthur@achaloin.com>
2019-06-11 16:54:04 +0200

    feat(admin): add setup.py for pip packaging

---
+++ admin/setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    name='bananadm',
+    version='0.1.0',
+    author='Arthur Chaloin from Enix <enix.io>',
+    author_email='arthur.chaloin@enix.fr',
+    description='A command line tool to manage Banana instances',
+    # long_description='',
+    # long_description_content_type='text/markdown',
+    # url='https://github.com/pypa/sampleproject',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
