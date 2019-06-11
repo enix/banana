ea3ecf417b1a2d519fb157eda1bd0c1a54a73f5a
Arthur Chaloin <arthur@achaloin.com>
2019-06-11 11:55:30 +0200

    feat(proxy): fetch CAs from vault and reload configuration on /reconfigure call

--- config/nginx/fetch_ca_certs.sh
+++ config/nginx/fetch_ca_certs.sh
@@ -1,17 +1,23 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 set -e
 
 out="$1"
-VAULT_TOKEN="$2"
+export VAULT_ADDR="$2"
+export VAULT_TOKEN="$3"
 
-vault token lookup > /dev/null
+vault token lookup -tls-skip-verify > /dev/null
 
-rm -f ${out}
-mounts=$(vault read /sys/mounts -format=json | jq -r '.data | with_entries(select(.key | match("banana.*pki"))) | keys[]')
+mounts=$(vault read -tls-skip-verify /sys/mounts -format=json | jq -r '.data | with_entries(select(.key | match("banana.*pki"))) | keys[]')
+if [ -z "${mounts}" ]; then
+	exit
+fi
+
+cp /dev/null ${out}
 for mount in ${mounts}; do
-	cacert=$(vault read ${mount}/cert/ca -format=json | jq -r '.data.certificate')
+	cacert=$(vault read -tls-skip-verify ${mount}/cert/ca -format=json | jq -r '.data.certificate')
 	echo "${cacert}" >> ${out}
 done
 
 echo 'wrote CA certs to' ${out}
+sudo /usr/local/openresty/nginx/sbin/nginx -s reload
