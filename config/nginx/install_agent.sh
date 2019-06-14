b568212e78a0deaa6f239b1008bd8f88cf384c0b
Arthur Chaloin <arthur@achaloin.com>
2019-06-14 17:35:27 +0200

    fix(conf): start timer instead of unit when installing the agent

--- config/nginx/install_agent.sh
+++ config/nginx/install_agent.sh
@@ -3,26 +3,26 @@
 set -e
 
 token="$1"
+ref="${2:-master}"
 
 function run {
 	echo $ "$@" >&2
 	$@
 }
 
-run cd /tmp
+cd /tmp
 
 echo "downloading latest agent release..."
 curl > agent.zip \
-	-fLH "Private-Token: $token" \
-	"https://gitlab.enix.io/api/v4/projects/166/jobs/artifacts/develop/download?job=build-agent-linux"
+	-fLH "Private-Token: ${token}" \
+	"https://gitlab.enix.io/api/v4/projects/166/jobs/artifacts/${ref}/download?job=build-agent-linux"
 
 run unzip -o agent.zip
 
 run cp bananactl-linux /usr/bin/bananactl
 run cp config/systemd/* /etc/systemd/system/
 
-run systemctl daemon-reload
-run systemctl start banana
-run systemctl enable banana
+run systemctl start banana.timer
+run systemctl enable banana.timer
 
 echo "success!"
