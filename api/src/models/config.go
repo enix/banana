f139a1dfb04515bdbc78d8f74b7b5eb42f34ee87
Arthur Chaloin <arthur@achaloin.com>
2019-06-14 16:44:13 +0200

    feat(agent): load separate schedule.json file

--- api/src/models/config.go
+++ api/src/models/config.go
@@ -19,24 +19,25 @@ import (
 // ScheduledBackupConfig : Contains informations of when to run inc/full backups, and config overrides
 type ScheduledBackupConfig struct {
 	Config
-	Target    string  `json:"target"`
-	Interval  float32 `json:"interval"`
-	FullEvery int     `json:"full_every"`
+	Target    string  `json:"target,omitempty"`
+	Interval  float32 `json:"interval,omitempty"`
+	FullEvery int     `json:"full_every,omitempty"`
 }
 
 // Config : Contains full confugration will be used to execute commands
 type Config struct {
-	MonitorURL       string                           `json:"monitor_url,omitempty"`
-	Backend          string                           `json:"backend,omitempty"`
-	StatePath        string                           `json:"state_path,omitempty"`
-	PrivKeyPath      string                           `json:"private_key_path,omitempty"`
-	CertPath         string                           `json:"client_cert_path,omitempty"`
-	BucketName       string                           `json:"bucket,omitempty"`
-	StorageHost      string                           `json:"storage_host,omitempty"`
-	SkipTLSVerify    bool                             `json:"skip_tls_verify"`
-	TTL              int64                            `json:"ttl,omitempty"`
-	Vault            services.VaultConfig             `json:"vault,omitempty"`
-	ScheduledBackups map[string]ScheduledBackupConfig `json:"schedule,omitempty"`
+	MonitorURL         string                           `json:"monitor_url,omitempty"`
+	Backend            string                           `json:"backend,omitempty"`
+	StatePath          string                           `json:"state_path,omitempty"`
+	PrivKeyPath        string                           `json:"private_key_path,omitempty"`
+	CertPath           string                           `json:"client_cert_path,omitempty"`
+	ScheduleConfigPath string                           `json:"schedule_config_path,omitempty"`
+	BucketName         string                           `json:"bucket,omitempty"`
+	StorageHost        string                           `json:"storage_host,omitempty"`
+	SkipTLSVerify      bool                             `json:"skip_tls_verify,omitempty"`
+	TTL                int64                            `json:"ttl,omitempty"`
+	Vault              services.VaultConfig             `json:"vault,omitempty"`
+	ScheduledBackups   map[string]ScheduledBackupConfig `json:"schedule,omitempty"`
 }
 
 // CliConfig : Extended config struct for stuff that can be passed from cli only
@@ -47,14 +48,15 @@ type CliConfig struct {
 // LoadDefaults : Prepare some default values in configuration
 func (config *Config) LoadDefaults() {
 	*config = Config{
-		MonitorURL:  "https://api.banana.enix.io",
-		Backend:     "duplicity",
-		StatePath:   "/etc/banana/state.json",
-		PrivKeyPath: "/etc/banana/privkey.pem",
-		CertPath:    "/etc/banana/cert.pem",
-		BucketName:  "backup-bucket",
-		StorageHost: "object-storage.r1.nxs.enix.io",
-		TTL:         3600,
+		MonitorURL:         "https://api.banana.enix.io",
+		Backend:            "duplicity",
+		StatePath:          "/etc/banana/state.json",
+		PrivKeyPath:        "/etc/banana/privkey.pem",
+		CertPath:           "/etc/banana/cert.pem",
+		ScheduleConfigPath: "/etc/banana/schedule.json",
+		BucketName:         "backup-bucket",
+		StorageHost:        "object-storage.r1.nxs.enix.io",
+		TTL:                3600,
 		Vault: services.VaultConfig{
 			Addr:              "http://localhost:7777",
 			StorageSecretPath: "openstack",
@@ -71,8 +73,19 @@ func (config *Config) LoadFromFile(path string) error {
 		klog.V(2).Info(err)
 		return err
 	}
-
 	json.Unmarshal(bytes, config)
+
+	if len(config.ScheduleConfigPath) > 0 {
+		bytes, err := ioutil.ReadFile(config.ScheduleConfigPath)
+		if err != nil {
+			klog.V(2).Info("warning: can't load schedule config file " + config.ScheduleConfigPath)
+			klog.V(2).Info(err)
+			return err
+		}
+
+		return json.Unmarshal(bytes, &config.ScheduledBackups)
+	}
+
 	return nil
 }
 
@@ -84,13 +97,14 @@ func (config *Config) LoadFromArgs(args *CliConfig) error {
 // LoadFromEnv : Load configuration from env variables
 func (config *Config) LoadFromEnv() error {
 	env := Config{
-		MonitorURL:  os.Getenv("BANANA_MONITOR_URL"),
-		Backend:     os.Getenv("BANANA_BACKEND"),
-		StatePath:   os.Getenv("BANANA_STATE_PATH"),
-		PrivKeyPath: os.Getenv("BANANA_PRIVATE_KEY_PATH"),
-		CertPath:    os.Getenv("BANANA_CLIENT_CERT_PATH"),
-		BucketName:  os.Getenv("BANANA_BUCKET_NAME"),
-		StorageHost: os.Getenv("BANANA_STORAGE_HOST"),
+		MonitorURL:         os.Getenv("BANANA_MONITOR_URL"),
+		Backend:            os.Getenv("BANANA_BACKEND"),
+		StatePath:          os.Getenv("BANANA_STATE_PATH"),
+		PrivKeyPath:        os.Getenv("BANANA_PRIVATE_KEY_PATH"),
+		CertPath:           os.Getenv("BANANA_CLIENT_CERT_PATH"),
+		ScheduleConfigPath: os.Getenv("BANANA_SCHEDULE_CONFIG_PATH"),
+		BucketName:         os.Getenv("BANANA_BUCKET_NAME"),
+		StorageHost:        os.Getenv("BANANA_STORAGE_HOST"),
 		Vault: services.VaultConfig{
 			Addr:              os.Getenv("VAULT_ADDR"),
 			RootPath:          os.Getenv("BANANA_VAULT_ROOT_PATH"),
