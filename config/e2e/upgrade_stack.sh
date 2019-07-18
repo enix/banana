f80fa080901cc76c2b608c8321406be455d1e45f
Arthur Chaloin <arthur@achaloin.com>
2019-07-18 15:59:10 +0200

    fix(ci): add debug for env

--- config/e2e/upgrade_stack.sh
+++ config/e2e/upgrade_stack.sh
@@ -7,6 +7,7 @@ sudo -i
 docker-compose -f /root/docker-compose.yml up -d
 
 # unseal vault
+env | grep VAULT
 echo vault operator unseal ${VAULT_UNSEAL_KEY} -tls-skip-verify
 vault operator unseal ${VAULT_UNSEAL_KEY} -tls-skip-verify
 
