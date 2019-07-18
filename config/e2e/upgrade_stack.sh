e059c0812fe9fd5670372d7c5ce301d028351b37
Arthur Chaloin <arthur@achaloin.com>
2019-07-18 16:23:07 +0200

    fix(ci): load env variables before upgrading stack

--- config/e2e/upgrade_stack.sh
+++ config/e2e/upgrade_stack.sh
@@ -2,13 +2,12 @@
 
 set -e
 sudo -i
+source ~/.ssh/environment
 
 # upgrade the stack
 docker-compose -f /root/docker-compose.yml up -d
 
 # unseal vault
-env | grep VAULT
-echo vault operator unseal ${VAULT_UNSEAL_KEY} -tls-skip-verify
 vault operator unseal ${VAULT_UNSEAL_KEY} -tls-skip-verify
 
 # reload nginx configuration
