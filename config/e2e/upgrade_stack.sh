43daedaaf0f3ec9870332c2966f3b7ebc75778e3
Arthur Chaloin <arthur@achaloin.com>
2019-07-18 15:34:13 +0200

    feat(ci): upgrade stack before e2e tests

---
+++ config/e2e/upgrade_stack.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+# upgrade the stack
+docker-compose -f /root/docker-compose.yml up -d
+
+# unseal vault
+echo ${VAULT_UNSEAL_KEY} | vault operator unseal -tls-skip-verify
+
+# reload nginx configuration
+bananadm --tls-skip-verify reconfigure
