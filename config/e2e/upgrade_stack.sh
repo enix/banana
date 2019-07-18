ea6a8d6110a2fe6df058b32e7e7a119a19083c6e
Arthur Chaloin <arthur@achaloin.com>
2019-07-18 16:42:41 +0200

    fix(ci): fix vault arguments

--- config/e2e/upgrade_stack.sh
+++ config/e2e/upgrade_stack.sh
@@ -9,7 +9,7 @@ cd ~
 docker-compose -f /root/docker-compose.yml up -d
 
 # unseal vault
-vault operator unseal ${VAULT_UNSEAL_KEY} -tls-skip-verify
+vault operator unseal -tls-skip-verify ${VAULT_UNSEAL_KEY}
 
 # reload nginx configuration
 bananadm --tls-skip-verify reconfigure
