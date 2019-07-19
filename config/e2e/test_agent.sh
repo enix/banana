48cc547d48292d17f90d54e490d5e0d38aa3f5ed
Arthur Chaloin <arthur@achaloin.com>
2019-07-19 14:58:56 +0200

    feat(ci): use freshly built binary for testing

--- config/e2e/test_agent.sh
+++ config/e2e/test_agent.sh
@@ -4,7 +4,8 @@ set -e
 
 echo $2
 # update the agent
-curl -fsSLk https://api.banana.enix.io/install | bash -s "$1" "$2"
+# curl -fsSLk https://api.banana.enix.io/install | bash -s "$1" "$2"
+cp /home/ubuntu/bananactl-linux /usr/local/bin/bananactl
 echo version that will be tested: $(bananactl version)
 
 cat /etc/banana/banana.json
