749cb9baf5f87dcd6b061d831c10b5804766ab6e
Arthur Chaloin <arthur@achaloin.com>
2019-07-19 14:43:49 +0200

    wip(ci): add more debug

--- agent/src/main.go
+++ agent/src/main.go
@@ -116,15 +116,19 @@ func main() {
 	}
 	err = config.LoadFromEnv()
 	assert(err)
+	fmt.Println(config.Vault)
 	err = config.LoadFromArgs(&args.Flags)
 	assert(err)
 
+	fmt.Println(config.Vault)
 	cmd, err := newCommand(args)
 	assert(err)
 
+	fmt.Println(config.Vault)
 	if args.Values[0] != "init" {
 		loadCredentialsToMem(config)
 	}
+	fmt.Println(config.Vault)
 	err = services.OpenVaultConnection(config.Vault, config.SkipTLSVerify)
 	assert(err)
 
