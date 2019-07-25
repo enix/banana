11afa1ebf45f053b2f5cde643f60f28209ec8ac6
Arthur Chaloin <arthur@achaloin.com>
2019-07-25 14:23:33 +0200

    refactor(config): move vault conf into a data container "settings"

--- config/vault/entrypoint.sh
+++ config/vault/entrypoint.sh
@@ -4,6 +4,8 @@ subject="/C=FR/ST=Ile-De-France/L=Paris/O=Enix/OU=Banana/CN=banana.dev.enix.io"
 cert="/tls/banana.dev.enix.io.pem"
 key="/tls/banana.dev.enix.io.key"
 
+apk add --update openssl
+
 openssl req \
 	-new \
 	-newkey rsa:4096 \
@@ -14,4 +16,7 @@ openssl req \
 	-keyout ${key} \
 	-out ${cert}
 
-vault server -config /vault.hcl
+mkdir -p /data && chown vault:vault /data
+mkdir -p /tls && chown vault:vault /tls
+
+vault server -config /settings/vault/vault.hcl
